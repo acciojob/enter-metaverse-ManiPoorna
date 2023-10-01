78effc10d86785029bf30ba73f99f587f7b7701b
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-10-01 10:57:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 const enterBtn = document.getElementById("enterBtn");
 enterBtn.addEventListener("click",()=>{
-	document.getElementById("status").innerHTML = `<h1>Entered Meteverse</h1>`
+	document.getElementById("status").innerHTML = `<h1>Entered Metaverse</h1>`
 })
